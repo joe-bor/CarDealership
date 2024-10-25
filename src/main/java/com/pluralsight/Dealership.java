f1ca0acc9d0fc840688317470e6b6720480b8f31
Joezari Borlongan <bor.joezari@gmail.com>
2024-10-25 11:04:26 -0700

    Phase1: Added Vehicle && Dealership class layout

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,86 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private List<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    // OTHER METHODS:
+    public List<Vehicle> getVehiclesByPrice(double min, double max){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model){
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByYear(int min, int max){
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByColor(String color){
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByMileage(int min, int max){
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByType(String vehicleType){
+        return null;
+    }
+
+    public List<Vehicle> getAllVehicles(){
+        return this.inventory;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+        this.getInventory().add(vehicle);
+    }
+
+    public void removeVehicle(Vehicle vehicle){}
+
+    // GETTERS & SETTERS:
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Vehicle> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(List<Vehicle> inventory) {
+        this.inventory = inventory;
+    }
+}
