d27743de711753deb8e4babc92e794e111a7db1c
Joezari Borlongan <bor.joezari@gmail.com>
2024-10-25 11:29:51 -0700

    Phase 2: Added DealershipFileManager and allowed it to create Dealership objects by reading a csv file

---
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -0,0 +1,42 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+public class DealershipFileManager {
+    public static void main(String[] args) {
+        getDealership();
+    }
+
+    public static Dealership getDealership() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("dealership1.csv"))) {
+
+            String input;
+            int lineCount = 0;
+            Dealership dealership = null;
+            while ((input = bufferedReader.readLine()) != null) {
+
+                // Line 1 => pipe-delimited info about the dealership
+                String[] dealerParts = input.split(Pattern.quote("|"));
+                if (lineCount == 0) {
+                    dealership = new Dealership(dealerParts[0], dealerParts[1], dealerParts[2]);
+                } else {
+                    // Line 2+ => vehicle information
+                    Vehicle vehicle = new Vehicle(Integer.parseInt(dealerParts[0]), Integer.parseInt(dealerParts[1]), dealerParts[2], dealerParts[3], dealerParts[4], dealerParts[5], Integer.parseInt(dealerParts[6]), Double.parseDouble(dealerParts[7]));
+                    dealership.addVehicle(vehicle);
+                }
+                lineCount++;
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return null;
+    }
+
+    public static void saveDealership(Dealership dealership) {
+        // TODO: Implement
+    }
+}
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -20,4 +20,18 @@ public class Vehicle {
         this.odometer = odometer;
         this.price = price;
     }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vin=" + vin +
+                ", year=" + year +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", vehicleType='" + vehicleType + '\'' +
+                ", color='" + color + '\'' +
+                ", odometer=" + odometer +
+                ", price=" + price +
+                '}';
+    }
 }
