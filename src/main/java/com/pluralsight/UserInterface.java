53113fee79dbd2424ec62b06c5cc4240022c5899
Joezari Borlongan <bor.joezari@gmail.com>
2024-10-25 15:52:08 -0700

    fix a bug regarding closing scanners

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 public class UserInterface {
 
     private Dealership dealership;
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public void display() {
         init();
 
-        Scanner scanner = new Scanner(System.in);
         boolean isRunning = true;
         do {
             System.out.println("""
@@ -30,7 +30,7 @@ public class UserInterface {
                     99 - Quit
                     
                     """);
-            String answer = scanner.nextLine().trim();
+            String answer = SCANNER.nextLine().trim();
 
             switch (answer) {
                 case "1" -> processGetByPriceRequest();
@@ -50,34 +50,101 @@ public class UserInterface {
             }
 
         } while (isRunning);
-        scanner.close();
+        SCANNER.close();
     }
 
     public void processGetByPriceRequest() {
+        System.out.print("What is the min price? ");
+        double minPrice = SCANNER.nextDouble();
+        SCANNER.nextLine();
+        System.out.print("What is the max price? ");
+        double maxPrice = SCANNER.nextDouble();
+        SCANNER.nextLine();
+
+        displayVehicles(this.dealership.getVehiclesByPrice(minPrice, maxPrice));
     }
 
     public void processGetByMakeModelRequest() {
+        System.out.print("What is the make? ");
+        String make = SCANNER.nextLine();
+        System.out.print("What is the model? ");
+        String model = SCANNER.nextLine();
+
+        displayVehicles(this.dealership.getVehiclesByMakeModel(make, model));
     }
 
     public void processGetByYearRequest() {
+        System.out.print("What is the min year? ");
+        int minYear = SCANNER.nextInt();
+        SCANNER.nextLine();
+        System.out.print("What is the max year? ");
+        int maxYear = SCANNER.nextInt();
+        SCANNER.nextLine();
+
+        displayVehicles(this.dealership.getVehicleByYear(minYear, maxYear));
     }
 
     public void processGetByColorRequest() {
+        System.out.print("What is the color? ");
+        String color = SCANNER.nextLine();
+
+        displayVehicles(this.dealership.getVehicleByColor(color));
     }
 
     public void processGetByMileageRequest() {
+        System.out.print("What is the min mileage? ");
+        int minMileage = SCANNER.nextInt();
+        SCANNER.nextLine();
+        System.out.print("What is the max mileage? ");
+        int maxMileage = SCANNER.nextInt();
+        SCANNER.nextLine();
+
+        displayVehicles(this.dealership.getVehicleByMileage(minMileage, maxMileage));
     }
 
     public void processGetByVehicleTypeRequest() {
+        System.out.print("What is the vehicle type? ");
+        String vehicleType = SCANNER.nextLine();
+
+        displayVehicles(this.dealership.getVehicleByType(vehicleType));
     }
 
     public void processGetAllVehiclesRequest() {
+        displayVehicles(this.dealership.getAllVehicles());
     }
 
     public void processAddVehicleRequest() {
+        System.out.print("What is the vehicle vin? ");
+        int vin = SCANNER.nextInt();
+        SCANNER.nextLine();
+        System.out.print("What is the vehicle year? ");
+        int year = SCANNER.nextInt();
+        SCANNER.nextLine();
+        System.out.print("What is the vehicle make? ");
+        String make = SCANNER.nextLine();
+        System.out.print("What is the vehicle model? ");
+        String model = SCANNER.nextLine();
+        System.out.print("What is the vehicle type? ");
+        String vehicleType = SCANNER.nextLine();
+        System.out.print("What is the vehicle color? ");
+        String color = SCANNER.nextLine();
+        System.out.print("What is the vehicle mileage? ");
+        int mileage = SCANNER.nextInt();
+        SCANNER.nextLine();
+        System.out.print("What is the vehicle price? ");
+        double price = SCANNER.nextDouble();
+        SCANNER.nextLine();
+
+        System.out.println("Adding new vehicle to inventory... ");
+        this.dealership.addVehicle(new Vehicle(vin, year, make, model, vehicleType, color, mileage, price));
+        DealershipFileManager dfm = new DealershipFileManager();
+        dfm.saveDealership(this.dealership);
+        System.out.println("Successfully added new vehicle!\n");
     }
 
     public void processRemoveVehicleRequest() {
+        // TODO: implement!!
+//        this.dealership.removeVehicle();
     }
 
     private void init() {
