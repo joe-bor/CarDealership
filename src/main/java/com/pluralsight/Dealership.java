de3637d9b8a35f1984c490ad4744b7e5e5544c8a
Joezari Borlongan <bor.joezari@gmail.com>
2024-10-25 14:05:12 -0700

    Implemented 'get Lists' method of Dealership

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -17,39 +17,47 @@ public class Dealership {
     }
 
     // OTHER METHODS:
-    public List<Vehicle> getVehiclesByPrice(double min, double max){
-        return null;
+    public List<Vehicle> getVehiclesByPrice(double min, double max) {
+        return this.getAllVehicles().stream().
+                filter(vehicle -> vehicle.getPrice() >= min && vehicle.getPrice() <= max).toList();
     }
 
-    public List<Vehicle> getVehiclesByMakeModel(String make, String model){
-        return null;
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return this.getAllVehicles().stream()
+                .filter(vehicle -> vehicle.getMake().contains(make) && vehicle.getModel().contains(model)).toList();
     }
 
-    public List<Vehicle> getVehicleByYear(int min, int max){
-        return null;
+    public List<Vehicle> getVehicleByYear(int min, int max) {
+        return this.getAllVehicles().stream()
+                .filter(vehicle -> vehicle.getYear() >= min && vehicle.getYear() <= max).toList();
     }
 
-    public List<Vehicle> getVehicleByColor(String color){
-        return null;
+    public List<Vehicle> getVehicleByColor(String color) {
+        return this.getAllVehicles().stream()
+                .filter(vehicle -> vehicle.getColor().contains(color)).toList();
     }
 
-    public List<Vehicle> getVehicleByMileage(int min, int max){
-        return null;
+    public List<Vehicle> getVehicleByMileage(int min, int max) {
+        return this.getAllVehicles().stream()
+                .filter(vehicle -> vehicle.getOdometer() >= min && vehicle.getOdometer() <= max).toList();
     }
 
-    public List<Vehicle> getVehicleByType(String vehicleType){
-        return null;
+    public List<Vehicle> getVehicleByType(String vehicleType) {
+        return this.getAllVehicles().stream()
+                .filter(vehicle -> vehicle.getVehicleType().contains(vehicleType)).toList();
     }
 
-    public List<Vehicle> getAllVehicles(){
+    public List<Vehicle> getAllVehicles() {
         return this.inventory;
     }
 
-    public void addVehicle(Vehicle vehicle){
+    public void addVehicle(Vehicle vehicle) {
         this.getInventory().add(vehicle);
     }
 
-    public void removeVehicle(Vehicle vehicle){}
+    public void removeVehicle(Vehicle vehicle) {
+        this.getInventory().remove(vehicle);
+    }
 
     // GETTERS & SETTERS:
     public String getName() {
